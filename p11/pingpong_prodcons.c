a3a1ea11e17be244cd268d4c3cc7396fa31b5b4b
arielscht <ariel-evaldt@hotmail.com>
2023-06-09 15:19:14 -0300

    Implement consumer and producer example

--- p11/pingpong_prodcons.c
+++ p11/pingpong_prodcons.c
@@ -1,6 +1,97 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ppos.h"
+
+#define BUFFER_CAPACITY 5
+
+semaphore_t s_buffer, s_items, s_slots;
+task_t producer_task1, producer_task2, producer_task3, consumer_task1, consumer_task2;
+
+typedef struct buffer_t
+{
+    int items[BUFFER_CAPACITY];
+    int head;
+    int tail;
+} buffer_t;
+
+buffer_t buffer;
+
+void buffer_init(buffer_t *buffer)
+{
+    buffer->head = 0;
+    buffer->tail = -1;
+};
+
+void add_item(buffer_t *buffer, int item)
+{
+    buffer->tail = (buffer->tail + 1) % BUFFER_CAPACITY;
+    buffer->items[buffer->tail] = item;
+}
+
+int remove_item(buffer_t *buffer)
+{
+    int item = buffer->items[buffer->head];
+    buffer->head = (buffer->head + 1) % BUFFER_CAPACITY;
+    return item;
+}
+
+int bounded_random(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
+
+void producer(void *arg)
+{
+    int item;
+
+    for (;;)
+    {
+        task_sleep(1000);
+        item = bounded_random(0, 99);
+
+        sem_down(&s_slots);
+        sem_down(&s_buffer);
+        add_item(&buffer, item);
+        sem_up(&s_buffer);
+        sem_up(&s_items);
+
+        printf("%s produced %d\n", (char *)arg, item);
+    }
+}
+
+void consumer(void *arg)
+{
+    int item;
+    for (;;)
+    {
+        sem_down(&s_items);
+        sem_down(&s_buffer);
+        item = remove_item(&buffer);
+        sem_up(&s_buffer);
+        sem_up(&s_slots);
+
+        printf("%s consumed %d\n", (char *)arg, item);
+        task_sleep(1000);
+    }
+}
 
 int main()
 {
-    return 0;
+    ppos_init();
+    srand(time(NULL));
+
+    buffer_init(&buffer);
+
+    sem_init(&s_buffer, BUFFER_CAPACITY);
+    sem_init(&s_items, 0);
+    sem_init(&s_slots, BUFFER_CAPACITY);
+
+    task_init(&producer_task1, producer, "p1");
+    task_init(&producer_task2, producer, "p2");
+    task_init(&producer_task3, producer, "p3");
+    task_init(&consumer_task1, consumer, "                  c1");
+    task_init(&consumer_task2, consumer, "                  c2");
+
+    task_exit(0);
 }
